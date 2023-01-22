a3b81cede00fb522b1c75e4c5f78ea60e145e014
Nifu <frzmit@gmail.com>
2023-01-22 19:32:48 +1000

    remove asset prefix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: false,
+  },
+  // assetPrefix: isProd ? '/adc2023.github.io/' : '',
+  assetPrefix: '',
+  images: {
+    unoptimized: true,
   },
 }
 
