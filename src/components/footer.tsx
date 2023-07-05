4a13a371722c5ef8ebdc048122f394ab5d11ff0c
Nifu <frzmit@gmail.com>
2023-07-05 15:06:26 +1000

    Add attribution

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,12 +2,8 @@ const Footer = () => {
   return (
     <footer className="max-w-[70em] mx-auto pt-8 pb-32 w-full">
       <div className="mx-5 flex flex-col-reverse md:flex-row lg:px-0 items-start justify-between text-gray-600">
-        <small className=""><span className="font-defaultSans">&copy;</span> 2023 Australasian Database Conference. <br className="block sm:hidden" />
-          All Rights Reserved.
+        <small className=""><span className="font-defaultSans">&copy;</span> 2023 Australasian Database Conference. All Rights Reserved. Cover photo by Dmitry Osipenko on Unsplash.  
         </small>
-        {/* <small className="mb-3 md:mb-0"><a className="hover:underline"
-            href="https://www.acm.org/about-acm/policy-against-harassment" target="_blank"
-            rel="noreferrer">Code of Conduct</a></small> */}
       </div>
     </footer>
   );
