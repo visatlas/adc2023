d51e008f973cb7cdd2ec250e0fb297c5c0c05bfa
Nifu <frzmit@gmail.com>
2023-10-10 21:09:24 +1000

    Update news

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -63,7 +63,7 @@ export default function Home() {
             <h2 className="uppercase text-3xl font-bold text-orange-600 tracking-wide mb-5 mt-12">News</h2>
             <ul className="list-disc list-inside space-y-5">
               <li>
-                <strong>10 Oct, 2023:</strong> &nbsp;<span className="font-semibold">The online proceedings for ADC 2023 <a className="underline" href="/ADC_2023_Program_Structure.pdf" target="_blank" rel="noreferrer">can be accessed from here</a>.</span>
+                <strong>10 Oct, 2023:</strong> &nbsp;<span className="font-semibold">The program structure for ADC 2023 <a className="underline" href="/ADC_2023_Program_Structure.pdf" target="_blank" rel="noreferrer">can be accessed from here</a>.</span>
               </li>
               <li>
                 <strong>9 Oct, 2023:</strong> &nbsp;Every accepted paper in the research or shepherding track needs one author registration. Authors of accepted papers, after completing the registration, can contact Hui Luo (<a className="underline" href="mailto:huil@uow.edu.au" target="_blank" rel="noreferrer">huil@uow.edu.au</a>) to seek an invitation letter (if needed). Please attach your registration completion evidence to facilitate the process.
